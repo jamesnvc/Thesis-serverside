89c662dd61bdf8f4cce315b6ca2fa2137c4f09c0
James N. V. Cash <james.nvc@gmail.com>
2012-01-27 13:29:20 -0500

    Adding docstrings to API methods

--- musashi/api_handler.py
+++ musashi/api_handler.py
@@ -3,37 +3,100 @@ import web
 from musashi import analytics
 
 
+# TODO: User accounts?
 class Api(object):
 
     db = None
     analyzer = analytics.Analyzer()
 
-    def get_catalog(self, args):
+    def get_catalog(self, _):
+        """Handle a request to get a list of the available tracks.
+
+        Returns:
+            A JSON list of tracks available.
+        """
         # TODO: Filter which the requester can see?
         tracks = self.db.select('tracks')
         return self.to_json(tracks)
 
     def get_preview(self, args):
+        """Handle a request to get previews of a given track or tracks.
+
+        Arguments:
+            - args: web input with a `tracks` field which is a JSON list of
+              track ids.
+
+        Returns:
+            A json list of track preview data.
+        """
         pass
 
     def get_full(self, args):
+        """Handle a request to get full versions of a given track or tracks.
+
+        Arguments:
+            - args: web input with a `tracks` field which is a JSON list of
+              track ids.
+
+        Returns:
+            A json list of full track data.
+        """
         pass
 
     def analyze_workout(self, args):
+        """Analyze a given workout.
+
+        - args: web input with a `tracks` field which is a JSON list of track
+                ids.
+        """
         track_ids = json.loads(args.tracks)
         tracks, analysis, fatigue = self.analyzer.perform_analysis(track_ids)
         return json.dumps({'analysis': analysis, 'fatigue': list(fatigue)})
 
     def recommend_tracks(self, args):
+        """Given a partial list of tracks, give a recommendation for the rest
+        of the workout.
+
+        Arguments:
+            - args: web input with a `tracks` field which is a JSON list of
+              track ids.
+
+        Returns:
+            A json list of track ids for the recommended workout.
+        """
         pass
 
     def export_workout(self, args):
+        """Request that the workout be exported into a pdf and movie.
+
+        Arguments:
+            - args: web input with a `tracks` field which is a JSON list of track ids.
+
+        Returns:
+            A URL where a zip file of the exported workout can be downloaded.
+        """
         pass
 
     def recieve_ratings(self, args):
+        """Upload a list of ratings the user has for the given tracks.
+
+        Arguments:
+            - args: web input with a `track_ratings` field which is a JSON list
+              of [track id, rating] pairs.
+
+        Returns:
+        """
         pass
 
     def recieve_history(self, args):
+        """Upload a workout & associate with the current user's history.
+
+        Arguments:
+            - args: web input with a `tracks` field which is a JSON list of
+              track ids.
+
+        Returns:
+        """
         pass
 
     methods = {
